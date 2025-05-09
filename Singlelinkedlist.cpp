2516ce2759e40032a93ae2ecc849072be1b4b758
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-09 09:17:33 +0700

    membuat perulangan while

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -48,4 +48,18 @@ public :
         nodebaru->next = current;
         previous->next = nodebaru;
     }
+    
+
+    
+
+    bool Search(int nim, Node **previous, Node **current){
+        *previous = START;
+        *current = START;
+
+        while ((*current != NULL)&&(nim != (*current)->noMhs)){
+            *previous = *current;
+            *current = (*current)->next;
+        }
+        return (*current != NULL);
+    }
 };
